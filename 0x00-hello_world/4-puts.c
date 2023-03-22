935788ac3310ecaacfe6d5d803a5ad88a20a5dd8
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-21 17:02:56 -0700

    edit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This is a short string
+ * Return: return value zero is possitive
+ */
 int main(void)
+
 {
 char mystr[] = "\"Programming is like building a multilingual puzzle";
 
