8d4114fff0ccfd4ddca445aa4e27f6f61e67a775
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-22 07:58:26 -0700

    edit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -13,7 +13,7 @@ int myTest = 5;
 
 if (myTest > 0)
 
-printf(with proper grammer, but the outcome is a piece of art ,);
+printf("with proper grammer, but the outcome is a piece of art ,");
 
 return (0);
 }
