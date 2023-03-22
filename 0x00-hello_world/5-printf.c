63f0907879fd09fe6513ab094e7039c348df38f9
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-22 07:22:27 -0700

    edit 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is a short if statement
+ * Return: positive zero return
+ */
+int main(void)
+int myTest = 5;
+
+if (myTest > 0)
+{
+printf(with proper grammer, but the outcome is a piece of art,);
+
+return (0);
+}
