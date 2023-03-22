b098d6687f95847c6314975768cebfb866a23b03
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-22 07:42:19 -0700

    edit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,11 +7,13 @@
  * Return: positive zero return
  */
 int main(void)
+
+{
 int myTest = 5;
 
 if (myTest > 0)
-{
-printf(with proper grammer, but the outcome is a piece of art,);
+
+printf(with proper grammer, but the outcome is a piece of art ,);
 
 return (0);
 }
