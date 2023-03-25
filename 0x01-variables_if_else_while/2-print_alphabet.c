84562b08dd93f1a359581a0237f58164890f9f76
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-25 16:41:12 -0700

    Print a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *printf - prints a-z
+ *Return: Positive
+ */
+int main(void)
+{
+
+	int n = a;
+
+	if (n < z)
+	printf("%d\n", n++);
+
+	return (0);
+}
