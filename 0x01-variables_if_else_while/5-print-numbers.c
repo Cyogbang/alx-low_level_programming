b29383b70f56261f59e2f40054fb7f92d26db1bf
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-26 10:03:05 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print-numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printf - prints numbers 1 to 9
+ * Return: positive
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	printf("%d", a);
+	a++;
+	}
+	printf("\n")
+
+	return (0);
+}
