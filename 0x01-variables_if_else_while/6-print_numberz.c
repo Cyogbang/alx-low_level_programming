042d5f93237df2592837f4392a75aaa57931dd5b
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-26 13:44:16 -0700

    Alphabetz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * putchar - adding '0' + n allows us print int.
+ * Return: positive
+ */
+
+int main(void)
+{
+int a = 0;
+
+while (a < 10)
+{
+putchar('0' + a);
+a++;
+}
+putchar("\n");
+
+return (0);
+}
