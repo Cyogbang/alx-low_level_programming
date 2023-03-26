5467d13b04ae464228a7c493bb63e38397ebd036
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-25 17:38:12 -0700

    Print a-z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *printf - prints a-z
+ *Return: Positive
+ */
+int main(void)
+{
+	int n = 'a && A';
+
+	while (n <= 'z && Z')
+		printf("%c", n++);
+		printf("\n");
+
+	return (0);
+}
