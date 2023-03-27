7921c07b2d52def08677363b372398227395068e
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-27 05:08:55 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print_string - prints a string
+ * print_string - prints a string
+ * @s: the string to be printed
+ *
+ * Return: positive
+ */
+
+void print_string(char *s);
+
+	int main(void)
+{
+	char s[] = "_putchar";
+
+	print_string(s);
+	putchar('\n');
+
+	return (0);
+}
+
+void print_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+{
+	putchar(s[i]);
+	i++;
+}
+}
