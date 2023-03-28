b54654289405d631a64460e9440b177ade14aeb9
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-28 02:31:46 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sgn.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints a sign
+ * @n: the sign to print
+ *
+ * Return: positive
+ */
+
+int print_sign(int n)
+{
+
+	if (n > '0')
+	{
+	putchar('+');
+	}
+	return (1);
+
+	else if (n == '0')
+	{
+	putchar('0' + 0)
+	}
+	return (0);
+
+	else
+	{
+	putchar('-');
+	}
+	return (-1);
+}
