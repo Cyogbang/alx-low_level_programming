be78385edbbb7d76eb97b967f8b4e55c61ed68e2
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-31 14:01:15 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,13 +5,20 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return: Always 0 || 15 || -15
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
-	for (a = 0; a - 1)
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (-1);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	for (a = 0 && 
+	return (0);
 }
