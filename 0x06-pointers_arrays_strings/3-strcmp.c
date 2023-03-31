760c01462f9f5df625561f997111c37f9d128749
Cyogbang <ogbangcyprian360@gmail.com>
2023-03-31 06:04:16 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,16 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	if (s1 == s2)
+	if (s1 > s2)
 	{
-		return (0);
+		return (-15);
 	}
-	else if (s1 > s2)
+	else if (s1 < s2)
 	{
 		return (15);
 	}
 	else
 	{
-		return (-15);
+		return (0);
 	}
 }
