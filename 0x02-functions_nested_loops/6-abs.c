c4fb56185d553668ff496dab4090c8f1396e3279
Cyogbang <ogbangcyprian360@gmail.com>
2023-04-02 12:13:52 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - compustes the ansolute value of an integer
+ * @n: the integer to check
+ *
+ * Return: the absolute value
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
